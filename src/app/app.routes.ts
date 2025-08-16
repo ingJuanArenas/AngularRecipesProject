baee2561e589623af5e65a74b1e1116a01ca67d2
ingJuanArenas <juanarenasing@gmail.com>
2025-08-16 10:30:14 -0500

    setting main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "recipes",
+        pathMatch:'full',
+    },{
+        path: "recipes",
+        loadChildren: () => import('./components/recipes-route').then(r => r.recipes_routes)
+    }
+];
