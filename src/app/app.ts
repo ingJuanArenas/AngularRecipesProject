7798c386af45ab6ef20ae0c89b4f36bd1c301acc
ingJuanArenas <juanarenasing@gmail.com>
2025-08-16 10:32:14 -0500

    working succesfully

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {  RecipeList } from "./components/recipe-list/recipe-list";
+import {   RecipesDetails } from "./components/recipes-details/recipes-details";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RecipeList, RecipesDetails],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('recetas');
+  protected readonly title = signal('recipes');
 }
